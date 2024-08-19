3b0b14ae8613d00cc051e22448a9416eacba4ed1
Vitor Bretz <vitorbretz28@gmail.com>
2024-08-19 19:14:49 -0300

    Update script.js
    att

---
+++ script.js
@@ -0,0 +1,17 @@
+let  digitalElement = document.querySelector(".digital");
+let sElement = document.querySelector(".p_s");
+let mElement = document.querySelector(".p_m");
+let hElement = document.querySelector(".p_h");
+
+function updateClock(){
+    let now = new Date();
+    let hour = now.getHours();
+    let minute = now.getMinutes();
+    let second = now.getSeconds();
+
+    digitalElement.innerHTML = `${hour}:${minute}:${second}`;
+
+}
+
+
+setInterval(updateClock, 1000);
